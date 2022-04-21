ea8867c0165c73960204faed25858075d746a832
Danila Makarichev <72694196+Donatello-M@users.noreply.github.com>
2022-04-21 03:04:53 +0300

    Update models.py

--- backend/tags/models.py
+++ backend/tags/models.py
@@ -4,14 +4,6 @@ from django.core.validators import ValidationError
 from django.db import models
 
 
-def hex_code_validator(value):
-    pattern = '^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$'
-    if not compile(pattern, value):
-        raise ValidationError(
-            'HEX-код должен состоять из 6 символов и начинаться с #'
-        )
-
-
 class Tag(models.Model):
     name = models.CharField(
         max_length=200,
