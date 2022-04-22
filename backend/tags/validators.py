362f3907404456071bb1e38014380715c39881a4
Danila Makarichev <72694196+Donatello-M@users.noreply.github.com>
2022-04-23 01:00:11 +0300

    Update validators.py

--- backend/tags/validators.py
+++ backend/tags/validators.py
@@ -1,11 +1,11 @@
-from re import compile
+from re import match
 
 from django.core.validators import ValidationError
 
 
 def hex_code_validator(value):
     pattern = '^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$'
-    if not compile(pattern, value):
+    if not match(pattern, value):
         raise ValidationError(
             'HEX-код должен состоять из 6 символов и начинаться с #'
         )
